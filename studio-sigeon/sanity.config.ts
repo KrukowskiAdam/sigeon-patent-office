eb23ec5afce8800c64f528ac332136cf77daebf6
adam <krukowski.adam@gmail.com>
2025-11-05 20:38:34 +0100

    Add Media plugin and remove Vision from Sanity Studio

--- studio-sigeon/sanity.config.ts
+++ studio-sigeon/sanity.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
+import {media} from 'sanity-plugin-media'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
 
@@ -13,7 +13,7 @@ export default defineConfig({
 
   plugins: [
     structureTool({structure}), 
-    visionTool()
+    media()
   ],
 
   schema: {
